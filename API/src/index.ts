16a012c7be3f1c59fa77a7539845f4eeb1f91dc7
pabloTVS <pablo@truevaluesoft.com>
2021-02-17 07:42:52 +0100

    productos

--- API/src/index.ts
+++ API/src/index.ts
@@ -4,10 +4,13 @@ import * as express from 'express';
 import * as cors from 'cors';
 import * as helmet from 'helmet';
 import routes from './routes';
-
+const morgan = require ('morgan');
+//certificado SSL
+//const https = require ('https');
+//const path = require ('path');
+//const fs = require ('fs');
+//fin SSL
 const PORT = process.env.PORT || 3000;
-
-var morgan = require ('morgan');
 
 createConnection()
   .then(async () => {
@@ -22,8 +25,19 @@ createConnection()
     app.use(express.json());
     // Routes
     app.use('/', routes);
+/*
+    const sslServer = https.createServer(
+      {
+        key: fs.readFileSync(path.join(__dirname, 'cert', 'privkey.pem')),
+        cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem')),
+        chain: fs.readFileSync(path.join(__dirname, 'cert', 'chain.pem')),
+      }
+    );*/
+    //console.log(__dirname);
+    //console.log(path.join(__dirname,'cert'));
 
     // start express server
     app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
+//      sslServer.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`))
   })
   .catch(error => console.log(error));
